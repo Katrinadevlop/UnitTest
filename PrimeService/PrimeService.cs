55cbb651344124b9b367218ce959bf1a1bedfa80
dywkat <katya.kolosovaaa@gmail.com>
2024-04-20 20:13:55 +0300

    Первый тест

---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int numberOne, int numberTwo)
+        {
+            if (numberOne % numberTwo == 0)
+            {
+                return true;
+            }
+            throw new NotImplementedException("Please create a test first.");
+        }
+    }
+}
